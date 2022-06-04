7988163338072a63cfd26e7d49ef85917f59be16
nandatritona <nandhatritona@gmail.com>
2022-06-04 21:28:07 +0700

    create welcome screen

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,5 +8,27 @@
 export default {
   name: "Home",
   components: {},
+  data() {
+    return {
+        welcomeScreen: {
+            title: "Welcome!",
+            blogPost: "Sedang Dalam Tahap Pengembangan !!",
+            welcomeScreen: true,
+            photo: "coding",
+        },
+        sampleBlogPost: [
+            {
+                title: 'This a filter blog post title!',
+                blogHtml: 'This a filter blog post title!',
+                blogCoverPhoto: 'beatiful-stories',
+            },
+            {
+                title: 'This a filter blog post title!',
+                blogHtml: 'This a filter blog post title!',
+                blogCoverPhoto: 'designed-for-everyone',
+            },
+        ],
+    };
+  },
 };
 </script>
