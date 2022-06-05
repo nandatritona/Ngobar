8731a4b7f4459fea39d01e5ea57ef96a83e7298f
nandatritona <nandhatritona@gmail.com>
2022-06-05 15:21:08 +0700

    Merapikan code

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,17 +1,17 @@
 <template lang="">
-  <div class="home">
-    <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
-    <BlogPost :post="welcomeScreen"/>
-    <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
-    <div class="blog-card-wrap">
-        <div class="container">
-            <h3>View More Recent Blogs</h3>
-            <div class="blog-cards">
-                <BlogCards :post="post" v-for="(post, index) in sampleBlogCards" :key="index" />
+    <div class="home">
+        <!-- <img alt="Vue logo" src="../assets/logo.png" /> -->
+        <BlogPost :post="welcomeScreen"/>
+        <BlogPost :post="post" v-for="(post, index) in sampleBlogPost" :key="index"/>
+        <div class="blog-card-wrap">
+            <div class="container">
+                <h3>View More Recent Blogs</h3>
+                <div class="blog-cards">
+                    <BlogCards :post="post" v-for="(post, index) in sampleBlogCards" :key="index" />
+                </div>
             </div>
         </div>
     </div>
-  </div>
 </template>
 
 <script>
@@ -19,36 +19,36 @@ import BlogPost from '../components/BlogPost';
 import BlogCards from '../components/BlogCard';
 
 export default {
-  name: "Home",
-  components: { BlogPost, BlogCards },
-  data() {
-    return {
-        welcomeScreen: {
-            title: "Welcome!",
-            blogPost: "Sedang Dalam Tahap Pengembangan !!",
-            welcomeScreen: true,
-            photo: "coding",
-        },
-        sampleBlogPost: [
-            {
-                title: 'This a filter blog post title!',
-                blogHtml: 'This a filter blog post title!',
-                blogCoverPhoto: 'beautiful-stories',
+    name: "Home",
+    components: { BlogPost, BlogCards },
+    data() {
+        return {
+            welcomeScreen: {
+                title: "Welcome!",
+                blogPost: "Sedang Dalam Tahap Pengembangan !!",
+                welcomeScreen: true,
+                photo: "coding",
             },
-            {
-                title: 'This a filter title2!',
-                blogHtml: 'This a filter title!',
-                blogCoverPhoto: 'designed-for-everyone',
-            },
-        ],
-        sampleBlogCards: [
-            { blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "Jun 5, 2022" },
-            { blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "Jun 5, 2022" },
-            { blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "Jun 5, 2022" },
-            { blogTitle: "Blog Card #4", blogCoverPhoto: "stock-4", blogDate: "Jun 5, 2022" },
-        ],
-    };
-  },
+            sampleBlogPost: [
+                {
+                    title: 'This a filter blog post title!',
+                    blogHtml: 'This a filter blog post title!',
+                    blogCoverPhoto: 'beautiful-stories',
+                },
+                {
+                    title: 'This a filter title2!',
+                    blogHtml: 'This a filter title!',
+                    blogCoverPhoto: 'designed-for-everyone',
+                },
+            ],
+            sampleBlogCards: [
+                { blogTitle: "Blog Card #1", blogCoverPhoto: "stock-1", blogDate: "Jun 5, 2022" },
+                { blogTitle: "Blog Card #2", blogCoverPhoto: "stock-2", blogDate: "Jun 5, 2022" },
+                { blogTitle: "Blog Card #3", blogCoverPhoto: "stock-3", blogDate: "Jun 5, 2022" },
+                { blogTitle: "Blog Card #4", blogCoverPhoto: "stock-4", blogDate: "Jun 5, 2022" },
+            ],
+        };
+    },
 };
 </script>
 
